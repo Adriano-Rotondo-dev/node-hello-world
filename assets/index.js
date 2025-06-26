c14a22af03c2bf660d4262fcc8ce7306c9c2463b
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-26 15:59:16 +0200

    Add .env-example file and update package.json watch script to include environment variables

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,20 @@
-const hello = "Hello Boolean"
+import chalk from "chalk";
+import axios from 'axios';
+console.log(axios.isCancel('axios is not working'));
 
-console.log(hello)
+const hello = "Hello Boolean";
+
+console.log(chalk.bold.green(hello));
+console.log(chalk.red.underline(process.env.secret_password));
+
+axios.get('https://lanciweb.github.io/demo/api/pictures/')
+.then ( response => {
+    const result = response.data;
+    console.log(result)
+})
+.catch(error => {
+    console.error(error)
+})
+.then ( ()=> {
+    console.log(chalk.bgYellow('The Ajax request with Axios is working'))
+})

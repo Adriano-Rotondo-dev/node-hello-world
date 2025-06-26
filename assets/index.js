5021c8eee987a0445445b4d2e25acf59aa0862bf
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-26 17:18:10 +0200

    Refactor greeting logic in index.js and fix typo in package.json watch script

--- assets/index.js
+++ assets/index.js
@@ -2,9 +2,12 @@ import chalk from "chalk";
 import axios from 'axios';
 console.log(axios.isCancel('axios is not working'));
 
-const hello = "Hello Boolean";
+const name = process.argv[2] ??  "World";
+console.log("Hello " + name)
+console.log(process.argv);
 
-console.log(chalk.bold.green(hello));
+
+console.log(chalk.bold.green(name));
 console.log(chalk.red.underline(process.env.secret_password));
 
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
@@ -15,6 +18,7 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
 .catch(error => {
     console.error(error.name)
 })
-.then ( ()=> {
+.finally ( ()=> {
     console.log(chalk.bgYellow('The Ajax request with Axios is working'))
 })
+
